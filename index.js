6e69b637fd65e1e74c0e37728928a8dce0467d7e
laurens <lauren.smi001@gmail.com>
2023-05-13 12:58:53 -0400

    refine index, shapes, svg

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
-import inquirer from "inquirer";
-import fs from 'fs';
-import { Circle, Square, Triangle } from './lib/shapes.js';
 
+const inquirer = require("inquirer");
+const fs = require('fs');
+const SVG = require('./lib/svg.js'); 
+const {Circle, Square, Triangle} = require('./lib/shapes.js');
 
+//Function for user prompts
+const prompts = function() {
+  inquirer.prompt ([
+    {
+      name: 'text',
+      type: 'input',
+      message: 'Enter text for the logo. THREE characters max.',
+      validate: (text) =>
+            text.length <= 3 ||
+            "Invalid entry. Must contain THREE characters or less",
+    },
+    {
+      name: 'textColor',
+      type: "input",
+      message: "Enter a text color",
+    },
+    {
+      name: "shapeType",
+      type: "list",
+      message: "Select a background shape for the logo",
+      choices: ["circle", "square", "triangle"],
+    },
+    {
+      name: "shapeColor",
+      type: "input",
+      message: "Enter a background shape color",
+    },
+  ])
+
+  .then(({ text, textColor, shapeType, shapeColor }) => {
+    let shape;
+    switch (shapeType) {
+      case "circle":
+        shape = new Circle();
+        break;
+
+      case "square":
+        shape = new Square();
+        break;
+
+      default:
+        shape = new Triangle();
+        break;
+    }
+    shape.setColor(shapeColor);
+
+    
+    const svg = new SVG();
+    svg.setText(text, textColor);
+    svg.setShape(shape);
+    fs.writeFile("logo.svg", svg.render(), (err) => {
+      if (err) throw err;
+      console.log("Generated logo.svg");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log("Oops! Something went wrong.");
+  });
+};
+  
+prompts();
